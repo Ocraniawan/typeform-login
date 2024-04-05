8dfc64263cf6d4f54b7085f7022dc711645e8caa
Ocraniawan Patattan <Ocraniawanpatattan@gmail.com>
2024-04-05 13:03:49 +0700

    feat: create left sections for desktop mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,8 +11,31 @@ export default function Home() {
     <>
       <HeadTag />
       <main className="block">
-        <div className="flex">
-          <section className="grid overflow-auto h-full p-0 rounded-t-md rounded-l-md flex-1-1-10 relative min-h-[90vh] w-full">
+        <div className="flex h-screen overflow-hidden">
+          <section className=" hidden lg:block bg-[#191919] flex-1 overflow-hidden relative flex-col m-h-[90vh] w-full p-0">
+            <div className="flex flex-col items-center h-full w-full justify-center">
+              <h1 className="text-white text-4xl mb-[35px] w-[366px] font-normal leading-[44px] text-center">
+                Sign Up <br /> and come on in
+              </h1>
+              <div>
+                <picture>
+                  <img
+                    alt="Typeform product sample"
+                    loading="lazy"
+                    decoding="async"
+                    fetchPriority="low"
+                    role="presentation"
+                    src="/images/product-sample.webp"
+                    className="w-full max-w-[366px]"
+                  />
+                </picture>
+              </div>
+              <p className="text-white text-sm leading-5 absolute bottom-6 mb-5">
+                © Typeform
+              </p>
+            </div>
+          </section>
+          <section className="lg:ml-[-16px] grid overflow-auto h-full p-0 rounded-t-2xl rounded-l-2xl flex-1-1-10 relative min-h-[90vh] w-full">
             <div className="bg-white grid grid-cols-custom grid-rows-custom h-screen w-full relative">
               <Languages />
               <Header />
